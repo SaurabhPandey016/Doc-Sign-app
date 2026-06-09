2212ff949f3ab9f939e70bc8dfa6a7f23817af7c
saurabh pandey <wesaysaurabh@gmail.com>
2026-06-09 14:19:25 +0530

    Day-11-work-done

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -41,7 +41,7 @@ export default function Navbar() {
             ) : (
               <>
                 <Link href="/" className="rounded-full px-3 py-2 text-xs font-semibold text-zinc-700 transition hover:bg-white hover:text-zinc-950 dark:text-zinc-200 dark:hover:bg-zinc-800 dark:hover:text-white">Home</Link>
-               {/* <Link href="/login" className="rounded-full px-3 py-2 text-xs font-semibold text-zinc-700 transition hover:bg-white hover:text-zinc-950 dark:text-zinc-200 dark:hover:bg-zinc-800 dark:hover:text-white">Sign In</Link> */}
+               <Link href="/login" className="rounded-full px-3 py-2 text-xs font-semibold text-zinc-700 transition hover:bg-white hover:text-zinc-950 dark:text-zinc-200 dark:hover:bg-zinc-800 dark:hover:text-white">Sign In</Link>
               </>
             )}
           </div>
@@ -65,18 +65,26 @@ export default function Navbar() {
               </div>
             ) : (
               <div className="hidden sm:flex items-center gap-2">
-                <Link
+                {/* <Link
                   href="/login"
                   className="inline-flex items-center gap-2 rounded-xl bg-zinc-950 px-4 py-2 text-xs font-semibold text-white shadow-lg shadow-zinc-900/10 transition hover:bg-zinc-800 dark:bg-white dark:text-zinc-950 dark:hover:bg-zinc-100"
                 >
                   Sign In
-                </Link>
+                </Link> */}
                 <Link
+                  href="/register"
+                  className="inline-flex items-center gap-2 rounded-xl bg-zinc-950 px-4 py-2 text-xs font-semibold text-white shadow-lg shadow-zinc-900/10 transition hover:bg-zinc-800 dark:bg-white dark:text-zinc-950 dark:hover:bg-zinc-100"
+                >
+                  Create Account
+                </Link>
+
+                {/* This is original register for previous styles prefer this */}
+                {/* <Link
                   href="/register"
                   className="inline-flex items-center gap-2 rounded-xl border border-zinc-200 bg-white px-4 py-2 text-xs font-semibold text-zinc-700 shadow-sm transition hover:bg-zinc-50 dark:border-zinc-800 dark:bg-zinc-950 dark:text-zinc-200 dark:hover:bg-zinc-900"
                 >
                   Register
-                </Link>
+                </Link> */}
               </div>
             )}
 
